7e80440321ba9c88b29446dd6ae0b2b2d0e0fc62
Alexander-Muendesi <u20426918@tuks.co.za>
2023-02-07 09:59:50 +0200

    ⚡️ Added React Router for routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './pages/errorPage/errorPage';
+import FeaturesPage from './pages/features/FeaturesPage';
+import LoginPage from './pages/login/LoginPage';
+import SignUpPage from './pages/signUp/SignUpPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",//router route
+    element: <App />,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/features",
+    element: <FeaturesPage/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage/>
+  }
+]);
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
